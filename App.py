71f10cd90899d91567f1a1a2f529bcd41600f85d
emilio barrera <barrera_emilio@yahoo.es>
2023-01-25 16:07:51 +0000

    Database & Tables

--- App.py
+++ App.py
@@ -1,17 +1,35 @@
-from flask  import Flask, render_template
-
-
-
-
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = "Secrect Key"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/crud1'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Data(db.Model):
+    Id = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.String(100))
+    Email = db.Column(db.String(100))
+    Phone = db.Column(db.String(100))
+
+    def __init__(self, name, email, phone):
+        self.Name = name
+        self.Email = email
+        self.Phone = phone
+
+
+with app.app_context():
+    db.create_all()
+
 
 @app.route('/')
 def Index():
-		return render_template("index.html")
+  return render_template("index.html")
 
 
-if  __name__ == "__main__":
-			app.run(debug=True)
-
-
+if __name__ == "__main__":
+  app.run(debug=True)
